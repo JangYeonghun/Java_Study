6a8017922e87554120df045956e51e6886902ad1
juhwan <juhwan0411@gmail.com>
2024-01-07 21:00:47 +0900

    챕터 2공부

---
+++ juhwan/Chapter02/src/Code02_lab.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Code02_lab {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		String Name,address;
+		int weight;
+		
+		System.out.println("## 택배 보내기입니다. 다음을 각각 입력하세요 ##");
+	}
+
+}
